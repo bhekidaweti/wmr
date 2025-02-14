ee88d669e3af693e5186022fe3c905127db2d24e
bheki daweti <bheki.daweti@gmail.com>
2025-02-14 18:43:43 +0200

    backend/db.js env

--- back-end/db.js
+++ back-end/db.js
@@ -1,14 +1,21 @@
 const { Pool } = require("pg");
 require('dotenv').config({ path: '../.env' });
 
-// PostgreSQL connection settings
-const pool = new Pool({
+// PostgreSQL connection settings local host
+/*const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: String(process.env.DB_PASSWORD),
     port: process.env.DB_PORT,            
-});
+}); */
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: { rejectUnauthorized: false }
+})
 pool.connect()
   .then(() => console.log("Connected to the scraper database"))
   .catch(err => console.error("Connection error", err.stack));
