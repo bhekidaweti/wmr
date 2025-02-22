3f8d460f928c6c14cbf2aaca09fa7022301d6076
bheki <bheki.daweti@gmail.com>
2025-02-22 20:53:30 +0200

    attempting to fix render env issue

--- front-end/src/Components/firebase.js
+++ front-end/src/Components/firebase.js
@@ -6,8 +6,8 @@ console.log("Auth Domain from ENV:", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN)
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,//|| "AIzaSyAjijdurxgJo_0jwZE9LzGhlvg8NFXjvYg",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,//|| "wmr-memes.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
